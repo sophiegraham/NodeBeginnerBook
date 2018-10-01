d3418b614ec8116d28ed3d1ff18ad04207d1fef5
Sophie Graham <sophiergraham1@gmail.com>
2018-09-30 23:48:03 -0700

    added router.js, (pg 27/54)

--- server.js
+++ server.js
@@ -16,10 +16,30 @@
 // }
 // http.createServer(onRequest).listen(8888);
 // console.log("Server has started.");
+// var http = require("http");
+// function start() {
+// function onRequest(request, response) {
+//     console.log("Request received.");
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.write("Hello World");
+//     response.end();
+// }
+
+// http.createServer(onRequest).listen(8888);
+// console.log("Server has started.");
+// }
+
+// exports.start = start;
+
 var http = require("http");
-function start() {
+var url = require("url");
+
+function start(route) {
 function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World");
     response.end();
