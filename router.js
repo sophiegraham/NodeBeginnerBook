05cd4e9cd6d2c347dffe115d1b4e220d46689e37
Sophie Graham <sophiergraham1@gmail.com>
2018-10-07 22:35:14 -0700

    POST

--- router.js
+++ router.js
@@ -1,9 +1,8 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) {
     console.log('About to route a request for ' + pathname);
     if(typeof handle[pathname] === 'function') {
-        handle[pathname](response);
-    } 
-    else {
+        handle[pathname](response, postData);
+    } else {
         console.log('No request handler found for ' + pathname);
         response.writeHead(404, { 'Content-Type': 'text/plain' });
         response.write('404 Not found');
@@ -12,4 +11,3 @@ function route(handle, pathname, response) {
 }
     
 exports.route = route;
-
