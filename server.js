a877c906048753c33aea28e0109c94b395346c6e
Sophie Graham <sophiergraham1@gmail.com>
2018-10-07 23:36:51 -0700

    all done :)git add -A

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
-var querystring = require("querystring");
+/* eslint-disable no-console */
 
-function start(response, postData) {
-    console.log('Request handler "start" was called.');
+var http = require('http');
+var url = require('url');
 
-    var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' +
-        '</html>';
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received.');
+        route(handle, pathname, response, request);
+    }
 
-    response.writeHead(200, { 'Content-Type': 'text/html'});
-    response.write(body);
-    response.end();
-}
-
-function upload(response, postData) {
-    console.log('Request handler "upload" was called.');
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('You/ve sent the text: ' +
-    querystring.parse(postData).text);
-    response.end();
+    http.createServer(onRequest).listen(8888);
+    console.log('Server has started.');
 }
 
 exports.start = start;
-exports.upload = upload;
