d74c0dba693c6ecd49b4c1a8f1c62170fdf8e0f0
Sophie Graham <sophiergraham1@gmail.com>
2018-10-07 21:46:20 -0700

    finished 'Responding request handlers with non-blocking operations'

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,41 +1,22 @@
-// function start() {
-//     console.log("Request handler 'start' was called.");
+var exec = require("child_process").exec;
 
-//     function sleep(milliSeconds) {
-//         var startTime = new Date().getTime();
-//         while (new Date().getTime() < startTime + milliSeconds);
-//     }
-    
-//     sleep(10000);
-//     return "Hello Start";
-// }
-    
-// function upload() {
-//     console.log("Request handler 'upload' was called.");
-//     return "Hello Upload";
-// }
-    
-//     exports.start = start;
-//     exports.upload = upload;
-var exec = require('child_process').exec;
+function start(response) {
+    console.log("Request handler 'start' was called.");
 
-function start() {
-    console.log('Request handler "start" was called.');
-    var content = 'empty';
-
-    exec("ls -lah", function (error, stdout, stderr) {
-        content = stdout;
-    });
+    exec("find /",
+        { timeout: 10000, maxBuffer: 20000*1024 },
         function (error, stdout, stderr) {
-            content = stdout;
-        }
-
-    return content;
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(stdout);
+            response.end();
+    });
 }
 
-function upload() {
-    console.log('Request handler "upload" was called.');
-    return 'Hello Upload';
+function upload(response) {
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello Upload");
+    response.end();
 }
 
 exports.start = start;
